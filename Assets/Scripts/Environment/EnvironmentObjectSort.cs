6bf260231ad6346a1b0983e6454599abe8b23fd6
bjornthefire <bjorntalbot@gmail.com>
2021-03-01 19:26:57 -0800

    Added in a village house, made a script to sort the layer based on where the player is walking, made the prefab for the house

---
+++ Assets/Scripts/Environment/EnvironmentObjectSort.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentObjectSort : MonoBehaviour
+{
+    public BoxCollider2D collider;
+    // Update is called once per frame
+    public void Start()
+    {
+       GetComponent<SpriteRenderer>().sortingOrder = 1;
+    }
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+       GetComponent<SpriteRenderer>().sortingOrder = 10; 
+    }
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+       GetComponent<SpriteRenderer>().sortingOrder = 1; 
+    }
+}
