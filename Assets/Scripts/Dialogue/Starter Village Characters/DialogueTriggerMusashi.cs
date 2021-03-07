db203144e3068c8eb943e9190f51f624350e5848
bjornthefire <bjorntalbot@gmail.com>
2021-03-06 21:08:18 -0800

    Added filler villager art and implement animations. Created a Tree for walls and a tree for walking around. Added a happiness particle effect. Implemented sword slash on Musashi duel

--- Assets/Scripts/Dialogue/Starter Village Characters/DialogueTriggerMusashi.cs
+++ Assets/Scripts/Dialogue/Starter Village Characters/DialogueTriggerMusashi.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class DialogueTriggerMusashi : DialogueTrigger
 {
 public GameObject arm;
+public GameObject screenEffect;
+public GameObject particles;
 public bool challengingDialogue = false;
     public override void Start()
     {
@@ -85,6 +87,7 @@ public bool challengingDialogue = false;
         buttonA.SetActive(true);
         buttonB.SetActive(true);
         combatScore.SetActive(true);
+        
     }
     public override void ButtonA()
     {
@@ -93,12 +96,14 @@ public bool challengingDialogue = false;
         combatScore.SetActive(false);
         GameDictionary.choiceDictionary["One Arm"] = true;
         arm.SetActive(true);
+        screenEffect.SetActive(true);
     }
     public override void ButtonB()
     {
         buttonA.SetActive(false);
         buttonB.SetActive(false);
         combatScore.SetActive(false);
+        particles.SetActive(true);
     }
 
 }
