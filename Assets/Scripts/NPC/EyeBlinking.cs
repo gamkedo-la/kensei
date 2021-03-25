d46ef41b1ab95093294bde923cdd4cd0f9022e5d
Christer Kaitila <breakdance@mcfunkypants.com>
2021-03-25 16:04:04 -0700

    experimental eye blinking coroutine added to fishing villagers

---
+++ Assets/Scripts/NPC/EyeBlinking.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBlinking : MonoBehaviour
+{
+    public float eyesOpenMinTime = 2f;
+    public float eyesOpenMaxTime = 5f;
+    public float eyesClosedMinTime = 0.1f;
+    public float eyesClosedMaxTime = 0.25f;
+
+    SpriteRenderer m_SpriteRenderer;
+
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine("Blinking");
+    }
+
+    IEnumerator Blinking() {
+        for(;;) {
+            float delay = 1f;
+            if (m_SpriteRenderer) {
+                // if eyes are open
+                if (!m_SpriteRenderer.enabled) {
+                    // fast blink
+                    m_SpriteRenderer.enabled = true;
+                    delay = Random.Range(eyesClosedMinTime,eyesClosedMaxTime);
+                } else { 
+                    // open and for longer
+                    m_SpriteRenderer.enabled = false;
+                    delay = Random.Range(eyesOpenMinTime,eyesOpenMaxTime);
+                }
+            }
+             
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
