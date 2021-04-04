dacc5273c5c8043a67291edf609fac8601a34414
Ryan G <rgaillard25@gmail.com>
2021-04-03 20:23:02 -0700

    start Item Descriptions + Names in Inventory UI - Buggy🐛

---
+++ Assets/Scripts/Items_Inventory/DisplayItemInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DisplayItemInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject item;
+    public GameObject player;
+    public GameObject panel;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log("'DISPLAY ITEM Info'");
+        item = player.GetComponent<PlayerController>().targetItem;
+        if (item)
+        {
+            UnityEngine.Debug.Log(item);
+            panel.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log("'Hide ITEM DESCRIPTION'");
+        //panel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Items_Inventory/DisplayItemName.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DisplayItemName : MonoBehaviour, IPointerEnterHandler
+{
+    public GameObject player;
+    public GameObject item;
+    // Item should auto update based on what item is in the slot, like it does for the each slot right now
+    public Text itemName;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log("'DISPLAY ITEM Name'");
+        //player.GetComponent<PlayerController>().checkItemInInventory(slot);
+        item = player.GetComponent<PlayerController>().targetItem;
+        UnityEngine.Debug.Log("item");
+        UnityEngine.Debug.Log(item);
+        if (item)
+        {
+            UnityEngine.Debug.Log(item);
+            itemName.text = player.GetComponent<ItemClass>().itemName;
+        }
+    }
+    /*
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log("'DISPLAY ITEM DESCRIPTION'");
+        //player.GetComponent<PlayerController>().checkItemInInventory(slot);
+        item = player.GetComponent<PlayerController>().targetItem;
+
+
+        if (item)
+        {
+            UnityEngine.Debug.Log(item);
+            itemName.text = player.GetComponent<ItemClass>().itemName;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log("'Hide ITEM DESCRIPTION'");
+    }
+    */
+}
