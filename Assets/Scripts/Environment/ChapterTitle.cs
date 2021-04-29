245e15bdc7877e11034fbb9ed2f29d6a39153925
bjornthefire <bjorntalbot@gmail.com>
2021-04-28 20:37:49 -0700

    Added in Chapter Titles!

---
+++ Assets/Scripts/Environment/ChapterTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChapterTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForTitle());
+    }
+
+    // Update is called once per frame
+
+
+    IEnumerator WaitForTitle()
+    {
+        yield return new WaitForSeconds(1f);
+        this.gameObject.SetActive(false);
+    }
+}
