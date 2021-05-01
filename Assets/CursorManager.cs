1dab6dc4efd25ce0480e4782e716a55b635a64b2
IanCher <IanCher@users.noreply.github.com>
2021-05-01 16:21:56 +0200

    Custom cursor - Can be changed from the game manager

---
+++ Assets/CursorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] Texture2D cursorImage = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(
+            cursorImage, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
