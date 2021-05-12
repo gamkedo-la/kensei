b92649d10e5a514d89cac551891904c7bed971a5
Luis Montaña <lmontana@live.com>
2021-05-12 17:42:23 -0500

    Created Pristine Village's Village Elder GrandDaughter's Code.

---
+++ Assets/Scripts/Dialogue/Pristine Village/PristineVEGrandDaughterDialogueScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PristineVEGrandDaughterDialogueScript : DialogueTrigger
+{
+    public override void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            inRange = true;
+            button.SetActive(true);
+            if (GameDictionary.choiceDictionary["Ronin Path"])
+            {
+                button.GetComponent<DialogueRun>().dialogue = Dialogues[0];
+                button.GetComponent<DialogueRun>().trigger = this;
+            }
+            else if (GameDictionary.choiceDictionary["Monk Path"])
+            {
+                button.GetComponent<DialogueRun>().dialogue = Dialogues[1];
+                button.GetComponent<DialogueRun>().trigger = this;
+            }
+            else if (GameDictionary.choiceDictionary["Samurai Path"])
+            {
+                button.GetComponent<DialogueRun>().dialogue = Dialogues[2];
+                button.GetComponent<DialogueRun>().trigger = this;
+            }
+        }
+    }
+}
