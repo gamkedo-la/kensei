20a34d314b35b2b824a40952eecb05116ba298cf
Luis Montaña <lmontana@live.com>
2021-05-12 16:06:59 -0500

    Created audio manager script with mute function

---
+++ Assets/Scripts/Game/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private bool isMuted;
+
+    private void Start() {
+        isMuted = false;
+    }
+    public void ToggleSound()
+    {
+        isMuted = !isMuted;
+        if (!isMuted)
+        {
+            AudioListener.volume = 1f;
+        }
+        else
+        {
+            AudioListener.volume = 0f;
+        }
+    }
+}
