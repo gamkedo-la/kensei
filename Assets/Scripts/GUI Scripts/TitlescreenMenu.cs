f6165695924353080ebc6280335963273122ced3
Christer Kaitila <breakdance@mcfunkypants.com>
2021-05-17 15:28:50 -0700

    Titlescreen/Main Menu WIP

---
+++ Assets/Scripts/GUI Scripts/TitlescreenMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitlescreenMenu : MonoBehaviour
+{
+    public string SceneNameToLoad;
+    public void playButtonClicked()
+    {
+        Debug.Log("Play Button Clicked.");
+        SceneManager.LoadScene(SceneNameToLoad, LoadSceneMode.Single);
+    }
+}
