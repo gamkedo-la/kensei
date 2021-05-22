b691e35d3726e97f53a4aff377391a2f1857c78f
bjornthefire <bjorntalbot@gmail.com>
2021-05-21 18:44:35 -0700

    Added in dialogue for Takuan Soho

---
+++ Assets/Scripts/Dialogue/Pristine Village/TakuanSohoDialogueTriggerPV.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakuanSohoDialogueTriggerPV : DialogueTrigger
+{
+    public GameObject player;
+    public GameObject rakusu;
+    private bool tookRakusu;
+    public override void Start()
+    {
+        button.SetActive(false);
+        buttonA.SetActive(false);
+        buttonB.SetActive(false);
+        combatScore.SetActive(false);
+    }
+
+    void Update()
+    {
+            if (GameDictionary.choiceDictionary["Monk Path"] && dialogueEnd && !tookRakusu)
+            {
+                DecisionDisplay("Take the Rakusu", "Don't take the Rakusu");
+            }
+    }
+
+    public override void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            player = collider.gameObject;
+            inRange = true;
+            button.SetActive(true);
+
+            if (GameDictionary.choiceDictionary["Samurai Path"])
+            {
+                //pick which Dialogue to run
+                button.GetComponent<DialogueRun>().dialogue = Dialogues[0];
+                button.GetComponent<DialogueRun>().trigger = this;
+            }
+
+            if (GameDictionary.choiceDictionary["Ronin Path"])
+            {
+                //pick which Dialogue to run
+                button.GetComponent<DialogueRun>().dialogue = Dialogues[1];
+                button.GetComponent<DialogueRun>().trigger = this;
+            }
+
+            if (GameDictionary.choiceDictionary["Monk Path"] && !tookRakusu)
+            {
+                //pick which Dialogue to run
+                button.GetComponent<DialogueRun>().dialogue = Dialogues[2];
+                button.GetComponent<DialogueRun>().trigger = this;
+            }
+
+            if (GameDictionary.choiceDictionary["Monk Path"] && tookRakusu)
+            {
+                //pick which Dialogue to run
+                button.GetComponent<DialogueRun>().dialogue = Dialogues[3];
+                button.GetComponent<DialogueRun>().trigger = this;
+            }
+        }
+    }
+
+    public override void OnTriggerExit2D(Collider2D collider)
+    {
+        inRange = false;
+        button.SetActive(false);
+        button.GetComponent<DialogueRun>().dialogue = null;
+        button.GetComponent<DialogueRun>().trigger = null;
+        dialogueEnd = false;
+        panel.SetActive(false);
+        buttonA.SetActive(false);
+        buttonB.SetActive(false);
+        combatScore.SetActive(false);
+    }
+
+    public override void DecisionDisplay(string buttonAText, string buttonBText)
+    {
+        this.buttonA.GetComponentInChildren<Text>().text = buttonAText;
+        this.buttonB.GetComponentInChildren<Text>().text = buttonBText;
+        this.buttonA.SetActive(true);
+        this.buttonB.SetActive(true);
+    }
+
+    public override void ButtonA()
+    {
+        player.GetComponent<PlayerController>().AddItem(rakusu);
+        this.buttonA.SetActive(false);
+        this.buttonB.SetActive(false);
+        tookRakusu = true;
+    }
+
+    public override void ButtonB()
+    {
+        this.buttonA.SetActive(false);
+        this.buttonB.SetActive(false);
+    }
+}
