4f14de7c68a72a6c5b1257f931afefaceb2d2d2d
bjornthefire <bjorntalbot@gmail.com>
2021-06-07 22:08:24 -0700

    Fixed bug where you could retrigger shigeie dialogue in stronhold

--- Assets/Scripts/Environment/ShigieColliderScript.cs
+++ Assets/Scripts/Environment/ShigieColliderScript.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 public class ShigieColliderScript : MonoBehaviour
 {
-public GameObject shigie;
+    public GameObject shigie;
 
     void Update()
     {
-        if(GameDictionary.choiceDictionary["Used Hidden Tunnel"])
+        if (GameDictionary.choiceDictionary["Used Hidden Tunnel"])
         {
             this.gameObject.SetActive(false);
         }
     }
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.CompareTag("Player"))
+        if (collider.CompareTag("Player"))
         {
-            collider.GetComponent<PlayerController>().movementLocked = true;
-            shigie.GetComponent<ShigieMovementScript>().targetPosition = this.gameObject;
-            shigie.GetComponent<ShigieMovementScript>().onSwitch = true;
+            if (!GameDictionary.choiceDictionary["Shigeie Walked"])
+            {
+                collider.GetComponent<PlayerController>().movementLocked = true;
+                shigie.GetComponent<ShigieMovementScript>().targetPosition = this.gameObject;
+                shigie.GetComponent<ShigieMovementScript>().onSwitch = true;
 
-                if(!GameDictionary.choiceDictionary["Monk Path"] && !GameDictionary.choiceDictionary["Ronin Path"])
+                if (!GameDictionary.choiceDictionary["Monk Path"] && !GameDictionary.choiceDictionary["Ronin Path"])
                 {
                     GameDictionary.Instance.UpdateEntry("Samurai Path", true);
                 }
+            GameDictionary.Instance.UpdateEntry("Shigeie Walked", true);
+            }
+            else
+            {
+                this.gameObject.SetActive(false);
+            }
         }
-    
-        if(collider.CompareTag("Shigie"))
+
+        if (collider.CompareTag("Shigie"))
         {
             collider.GetComponent<IshidaShigieDialogueTrigger>().reachedEntrance = true;
         }
