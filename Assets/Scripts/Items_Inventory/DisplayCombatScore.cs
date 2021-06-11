0cc194c3dfc1d762a13e28cd4b9e575c40327560
bjornthefire <bjorntalbot@gmail.com>
2021-06-10 17:23:07 -0700

    Fixed combat score display

--- Assets/Scripts/Items_Inventory/DisplayCombatScore.cs
+++ Assets/Scripts/Items_Inventory/DisplayCombatScore.cs
@@ -6,11 +6,9 @@ using UnityEngine.UI;
 public class DisplayCombatScore : MonoBehaviour
 {
     public Text text;
-    public int combatScore;
     public GameObject player;
     void Update()
     {
-        combatScore = player.GetComponent<StateTracker>().combatScore;
-        text.text = combatScore.ToString();
+        text.text = player.GetComponent<StateTracker>().combatScore.ToString();
     }
 }
--- Assets/Scripts/Player/StateTracker.cs
+++ Assets/Scripts/Player/StateTracker.cs
@@ -13,7 +13,7 @@ public class StateTracker : MonoBehaviour
     public int currentBigItemPoints;
     public GameObject currentSmallItem;
     public int currentSmallItemPoints;
-    public int playerCombatPoints = 10; 
+    public int playerCombatPoints = 10;
     public int combatScore;
     public bool oneArm = false;
     public bool incrementedCombatPoints = false;
@@ -22,47 +22,47 @@ public class StateTracker : MonoBehaviour
     {
         oneArm = GameDictionary.choiceDictionary["One Arm"];
 
-        if(!player.GetComponent<PlayerController>().weapon)
+        if (!player.GetComponent<PlayerController>().weapon)
         {
-        currentWeaponPoints = 0;
-        currentWeapon = null;
+            currentWeaponPoints = 0;
+            currentWeapon = null;
         }
-        else 
+        else
         {
-        currentWeapon = player.GetComponent<PlayerController>().weapon;
-        currentWeaponPoints = currentWeapon.GetComponent<ItemClass>().combatPoints;
-        Debug.Log(currentWeapon.GetComponent<ItemClass>().itemName +" "+currentWeapon.GetComponent<ItemClass>().combatPoints);
+            currentWeapon = player.GetComponent<PlayerController>().weapon;
+            currentWeaponPoints = currentWeapon.GetComponent<ItemClass>().combatPoints;
+            Debug.Log(currentWeapon.GetComponent<ItemClass>().itemName + " " + currentWeapon.GetComponent<ItemClass>().combatPoints);
         }
 
-        if(!player.GetComponent<PlayerController>().clothing) 
+        if (!player.GetComponent<PlayerController>().clothing)
         {
             currentClothingPoints = 0;
             currentClothing = null;
         }
-        else 
+        else
         {
-        currentClothing = player.GetComponent<PlayerController>().clothing;
-        currentClothingPoints = currentClothing.GetComponent<ItemClass>().combatPoints;
-        Debug.Log(currentClothing.GetComponent<ItemClass>().itemName +" "+currentClothing.GetComponent<ItemClass>().combatPoints);
+            currentClothing = player.GetComponent<PlayerController>().clothing;
+            currentClothingPoints = currentClothing.GetComponent<ItemClass>().combatPoints;
+            Debug.Log(currentClothing.GetComponent<ItemClass>().itemName + " " + currentClothing.GetComponent<ItemClass>().combatPoints);
         }
 
-        if(!player.GetComponent<PlayerController>().bigItem) currentBigItemPoints = 0;
-        else 
+        if (!player.GetComponent<PlayerController>().bigItem) currentBigItemPoints = 0;
+        else
         {
-        currentBigItem = player.GetComponent<PlayerController>().bigItem;
-        currentBigItemPoints = currentBigItem.GetComponent<ItemClass>().combatPoints;
-        Debug.Log(currentBigItem.GetComponent<ItemClass>().itemName +" "+currentBigItem.GetComponent<ItemClass>().combatPoints);
+            currentBigItem = player.GetComponent<PlayerController>().bigItem;
+            currentBigItemPoints = currentBigItem.GetComponent<ItemClass>().combatPoints;
+            Debug.Log(currentBigItem.GetComponent<ItemClass>().itemName + " " + currentBigItem.GetComponent<ItemClass>().combatPoints);
         }
 
-        if(!player.GetComponent<PlayerController>().smallItem) currentSmallItemPoints = 0;
-        else 
+        if (!player.GetComponent<PlayerController>().smallItem) currentSmallItemPoints = 0;
+        else
         {
-        currentSmallItem = player.GetComponent<PlayerController>().smallItem;
-        currentSmallItemPoints = currentSmallItem.GetComponent<ItemClass>().combatPoints;
-        Debug.Log(currentSmallItem.GetComponent<ItemClass>().itemName +" "+currentSmallItem.GetComponent<ItemClass>().combatPoints);
+            currentSmallItem = player.GetComponent<PlayerController>().smallItem;
+            currentSmallItemPoints = currentSmallItem.GetComponent<ItemClass>().combatPoints;
+            Debug.Log(currentSmallItem.GetComponent<ItemClass>().itemName + " " + currentSmallItem.GetComponent<ItemClass>().combatPoints);
         }
-        
-        if(oneArm) combatScore = (currentWeaponPoints + currentClothingPoints + currentBigItemPoints + currentSmallItemPoints + playerCombatPoints) / 2;
+
+        if (oneArm) combatScore = (currentWeaponPoints + currentClothingPoints + currentBigItemPoints + currentSmallItemPoints + playerCombatPoints) / 2;
         else combatScore = currentWeaponPoints + currentClothingPoints + currentBigItemPoints + currentSmallItemPoints + playerCombatPoints;
         Debug.Log(combatScore);
     }
